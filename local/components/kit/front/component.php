9673ffb49c077bb02a193b5e10f470f00ef1b008
sasha23rus <sasha233rus@gmail.com>
2025-06-25 15:18:45 +0300

    del front component

--- local/components/kit/front/component.php
+++
@@ -1,9 +0,0 @@
-<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
-
-	/** @var array $arParams */
-
-	/*
-	 * Компонент для вывода разных блоков шаблона
-	 * */
-
-	$this->IncludeComponentTemplate();
