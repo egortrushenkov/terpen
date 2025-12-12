e18ea8b410bc0074e68b04ba2431a0e7c06ad94c
sasha23rus <sasha233rus@gmail.com>
2025-12-12 15:39:30 +0300

    add menu in header

--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -27,3 +27,17 @@
 		<div id="panel">
 			<?$APPLICATION->ShowPanel();?>
 		</div>
+
+		<?$APPLICATION->IncludeComponent("bitrix:menu","custom",Array(
+                "ROOT_MENU_TYPE" => "top",
+                "MAX_LEVEL" => "2",
+                "CHILD_MENU_TYPE" => "left",
+                "USE_EXT" => "N",
+                "DELAY" => "N",
+                "ALLOW_MULTI_SELECT" => "N",
+                "MENU_CACHE_TYPE" => "N",
+                "MENU_CACHE_TIME" => "3600",
+                "MENU_CACHE_USE_GROUPS" => "N",
+                "MENU_CACHE_GET_VARS" => ""
+            )
+        );?>
